1c3da5d532a70e33a5a143f7516d35c31bac67bb
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-01-05 16:27:16 -0300

    Inserindo a imagem dos produtos na listagem e mudando a aparência da lista

--- avancado/public/unidade_04/listagem.php
+++ avancado/public/unidade_04/listagem.php
@@ -1,6 +1,6 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 <?php
-	$produtos = "SELECT produtoID, nomeproduto, tempoentrega, precounitario";
+	$produtos = "SELECT produtoID, nomeproduto, tempoentrega, precounitario, imagempequena";
 	$produtos .= " FROM produtos";
 	$resultado = mysqli_query($conexao, $produtos);
 	
@@ -10,7 +10,7 @@
 ?>
 
 <!doctype html>
-<html>
+<html lang="pt-BR">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,23 +18,27 @@
 		
 		<!-- estilo -->
 		<link href="_css/estilo.css" rel="stylesheet">
+		<link href="_css/produtos.css" rel="stylesheet">
 	</head>
 
 	<body>
 		<?php include_once("../_incluir/topo.php"); ?>
 		<?php include_once("../_incluir/funcoes.php"); ?>
 		<main>
-			<?php
-				while($linha = mysqli_fetch_assoc($resultado)) {
-			?>
-				<ul>
-					<li><?php echo $linha["nomeproduto"] ?></li>
-					<li><?php echo $linha["precounitario"] ?></li>
-					<li><?php echo $linha["tempoentrega"] ?></li>
-				</ul>
-			<?php
-				}
-			?>
+			<div id="listagem_produtos">
+				<?php
+					while($linha = mysqli_fetch_assoc($resultado)) {
+				?>
+					<ul>
+						<li class="imagem"><img src="<?php echo $linha["imagempequena"] ?>"></li>
+						<li><h3><?php echo $linha["nomeproduto"] ?></h3></li>
+						<li>Preço unitário: <?php echo real_format($linha["precounitario"]) ?></li>
+						<li>Tempo entrega: <?php echo $linha["tempoentrega"] ?></li>
+					</ul>
+				<?php
+					}
+				?>
+			</div>
 		</main>
 		<?php include_once("../_incluir/rodape.php"); ?> 
 	</body>
