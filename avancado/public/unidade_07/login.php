626ba120b1fd64eadfc845ba8a42e8c833b2b315
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-01-07 16:11:14 -0300

    implementando a busca do login no banco

--- avancado/public/unidade_07/login.php
+++ avancado/public/unidade_07/login.php
@@ -1,4 +1,27 @@
 <?php require_once("../../conexao/conexao.php"); ?>
+<?php
+	if(isset($_POST["submit"]) && $_POST["submit"] == "login") {
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+
+    $login = "SELECT *";
+    $login .= " FROM clientes";
+    $login .= " WHERE usuario = '{$usuario}'";
+    $login .= " and senha = '{$senha}'";
+
+    $acesso = mysqli_query($conexao, $login);
+    if(!$acesso) {
+      $erro_busca = "Falha no acesso ao banco";
+    }
+
+    $informacao = mysqli_fetch_assoc($acesso);
+    if(empty($informacao)) {
+      $erro_busca = "Autenticação inválida";
+    } else {
+      header("location: listagem.php");
+    }
+  }
+?>
 <!doctype html>
 <html>
 	<head>
@@ -19,7 +42,15 @@
           <h2>Tela de Login</h2>
           <input type="text" name="usuario" placeholder="Usuário">
           <input type="password" name="senha" placeholder="Senha">
-          <input type="submit" value="login">
+          <input type="submit" name="submit" value="login">
+
+          <?php
+            if(isset($erro_busca)) {
+          ?>
+            <p><?php echo $erro_busca ?></p>
+          <?php
+            }
+          ?>
         </form>
       </div>
 		</main>
