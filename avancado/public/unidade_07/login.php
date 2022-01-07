4fe2a70400b539d50c04961b59dfaf4254416dcc
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-01-07 15:21:06 -0300

    criando a estrutura inicial da página de login

--- avancado/public/unidade_07/login.php
+++ avancado/public/unidade_07/login.php
@@ -7,18 +7,21 @@
 		
 		<!-- estilo -->
 		<link href="_css/estilo.css" rel="stylesheet">
+		<link href="_css/login.css" rel="stylesheet">
 	</head>
 
 	<body>
 		<?php include_once("../_incluir/topo.php"); ?>
 		<?php include_once("../_incluir/funcoes.php"); ?>
 		<main>
-      <?php
-        
-      ?>
-		<p>
-			<a href="pagina2.php">Página 2</a>
-		</p>
+      <div id="janela_login">
+        <form action="login.php" method="post">
+          <h2>Tela de Login</h2>
+          <input type="text" name="usuario" placeholder="Usuário">
+          <input type="password" name="senha" placeholder="Senha">
+          <input type="submit" value="login">
+        </form>
+      </div>
 		</main>
 		<?php include_once("../_incluir/rodape.php"); ?> 
 	</body>
