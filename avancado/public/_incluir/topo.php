27d31c6f2b25eb4f17596168b15d58bcd28599ae
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-01-10 17:02:45 -0300

    criando a mensagem de saudação e o link de logout no header

--- avancado/public/_incluir/topo.php
+++ avancado/public/_incluir/topo.php
@@ -1,6 +1,31 @@
 <header>
-    <div id="header_central">
-        <img src="../_assets/logo_andes.gif">
-        <img src="../_assets/text_bnwcoffee.gif">
-    </div>
+	<div id="header_central">
+		<?php
+			if(isset($_SESSION["user_portal"])) {
+				$user = $_SESSION["user_portal"];
+
+				$query = "SELECT *";
+				$query .= " FROM clientes";
+				$query .= " WHERE clienteID = '{$user}'";
+		
+				$saudacao_login = mysqli_query($conexao, $query);
+				if(!$saudacao_login) {
+					$erro_busca = "Falha no acesso ao banco";
+					$nome = "";
+				} else {
+					$saudacao_info = mysqli_fetch_assoc($saudacao_login);
+					$nome = $saudacao_info["nomecompleto"];
+				}
+		?>
+			<div id="header_saudacao">
+				<h5>
+					Bem vindo, <?php echo $nome ?> - <a href="sair.php">Sair</a>
+				</h5>
+			</div>
+		<?php
+			}
+		?>
+		<img src="../_assets/logo_andes.gif">
+		<img src="../_assets/text_bnwcoffee.gif">
+	</div>
 </header>
